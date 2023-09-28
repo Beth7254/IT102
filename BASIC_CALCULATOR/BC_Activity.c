bf753404baf639bc666164b5363469bc56d71a73
Beth7254 <marybeth.gracia@dbtc-cebu.edu.ph>
2023-09-28 10:18:02 +0800

    INITIAL COMMIT

--- BASIC_CALCULATOR/BC_Activity.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <conio.h>
-
-int main()
-{
-    int first, second, add , subtract, multiply;
-}
--- Functions/FUNCTION_SAMPLE/Activity.c
+++ Functions/FUNCTION_SAMPLE/Activity.c
@@ -1,15 +1,65 @@
-#include<stdio.h>
-#include<conio.h>
-#include<windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
 
 void gotoxy(int x, int y);
 void mainMenu();
 
-void main()
+int main()
 {
     system("chcp 65001 >null");
-    mainMenu();
-    getch();
+    int choice, num1, num2, result;
+    char go;
+
+    do
+    {
+        system("cls");
+        mainMenu();
+
+        printf("Choose an operation (1-5): ");
+        scanf("%d", &choice);
+
+        if (choice == 5)
+        {
+            break;
+        }
+
+        printf("Enter two numbers: ");
+        scanf("%d %d", &num1, &num2);
+
+        switch (choice)
+        {
+        case 1:
+            result = num1 + num2;
+            printf("Result: %d\n", result);
+            break;
+
+        case 2:
+            result = num1 - num2;
+            printf("Result: %d\n", result);
+            break;
+
+        case 3:
+            result = num1 * num2;
+            printf("Result: %d\n", result);
+            break;
+
+        case 4:
+            result = num1 / num2;
+            printf("Result: %d\n", result);
+            break;
+
+        default:
+            printf("Invalid choice\n");
+            break;
+        }
+
+        printf("Do you want to continue? (y/n): ");
+        scanf(" %c", &go);
+
+    } while (go == 'y');
+
+    return 0;
 }
 
 void mainMenu()
@@ -21,11 +71,12 @@ void mainMenu()
     printf("║ 2. Subtraction       ║\n");
     printf("║ 3. Multiplication    ║\n");
     printf("║ 4. Division          ║\n");
-    printf("║ 5. Exit              ║\n"); 
-    printf("╚══════════════════════╝\n");             
+    printf("║ 5. End Program       ║\n");
+    printf("╚══════════════════════╝\n");
 }
+
 void gotoxy(int x, int y)
 {
-    COORD c = { x, y };  
-    SetConsoleCursorPosition(  GetStdHandle(STD_OUTPUT_HANDLE) , c);
+    COORD c = {x, y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
