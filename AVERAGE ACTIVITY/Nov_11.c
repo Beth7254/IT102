c03d65b78690574a808ebce90e00cb92a3381cdb
Beth7254 <marybeth.gracia@dbtc-cebu.edu.ph>
2023-11-18 09:20:03 +0800

    New Commit

--- AVERAGE ACTIVITY/Nov_11.c
+++ AVERAGE ACTIVITY/Nov_11.c
@@ -1,49 +1,52 @@
+#include<stdio.h>
+#include<math.h>
+
+void sortArray(int array[20]);
 
 
-#include <stdio.h>
-
-void swap(int *xp, int *yp) {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-void bubbleSort(int arr[], int n) {
-   int i, j;
-   for (i = 0; i < n-1; i++)       
-       for (j = 0; j < n-i-1; j++) 
-           if (arr[j] < arr[j+1])
-              swap(&arr[j], &arr[j+1]);
-}
-
-double findAverage(int arr[], int n) {
-    int i;
-    double sum = 0;
-    for (i = 0; i < n; i++)
-        sum += arr[i];
-    return sum / n;
-}
-
-int main() {
-    int minutes[20];
-    int i;
-    double average;
-
-    // fill the array with minutes data
-    for (i = 0; i < 20; i++) {
-        minutes[i] = rand() % 100 + 1;
+void main()
+{
+    system("cls");
+    int myArray[20];
+    int i = 0;
+    float average = 0;
+    
+    for(i=0; i < 20; i ++)
+    {
+        printf("Please enter number for index %d: ", i);
+        scanf("%d", &myArray[i]);
+        average += myArray[i];
     }
 
-    bubbleSort(minutes, 20);
+    average = (average / 20);
 
-    average = findAverage(minutes, 20);
+    sortArray(myArray);
 
-    printf("Sorted array in descending order: \n");
-    for (i = 0; i < 20; i++)
-        printf("%d ", minutes[i]);
-    printf("\n");
+    printf("\nThe average is %.2f", average);
 
-    printf("Average time: %.2f\n", average);
+}
 
-    return 0;
+void sortArray(int array[20])
+{
+    int i = 0, j = 0, temp = 0;
+
+    for(i = 0; i < 20; i++)
+    {
+        for(j = i; j < 20; j ++)
+        {
+            if(array[j] > array[i])
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+
+    printf("\nThe sorted values: \n");
+
+    for(i = 0; i < 20; i++)
+    {
+        printf("%d ", array[i]);
+    }
 }
---
+++ BACKGROUND_COLOR/Activity.c
@@ -0,0 +1,139 @@
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+
+void mainMenu();
+void pattern();
+void series();
+void calculator();
+void endProgram();
+void gotoxy(int x, int y);
+
+void main()
+{
+    int choice = 0;
+    int index = 1;
+    system("chcp 65001 >null");
+    system("cls");
+    mainMenu();
+    pattern();
+    while (1) // up-H | right-M | down-P | left-K
+    {
+        choice = getch();
+
+        switch (choice)
+        {
+        case 80:
+            if (index >= 4)
+                index = 4;
+            else
+                index++;
+            break;
+
+        case 72:
+            if (index <= 1)
+                index = 1;
+            else
+                index--;
+            break;
+
+        case 13:
+            switch (index)
+            {
+            case 1:
+                printf("     pattern");
+                break;
+            case 2:
+                printf("     series");
+                break;
+            case 3:
+                printf("     calculator");
+                break;
+            case 4:
+                exit(0);
+                break;
+            }
+            break;
+        }
+
+        mainMenu();
+
+        switch (index)
+        {
+        case 1:
+            pattern();
+            break;
+        case 2:
+            series();
+            break;
+        case 3:
+            calculator();
+            break;
+        case 4:
+            endProgram();
+            break;
+        }
+    }
+}
+
+void endProgram()
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_GREEN);
+    gotoxy(48, 11);
+    printf(" >  End Program       ");
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BLACK_PEN);
+}
+
+void calculator()
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_GREEN);
+    gotoxy(48, 10);
+    printf(" >  Calculator        ");
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BLACK_PEN);
+}
+
+void series()
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_GREEN);
+    gotoxy(48, 9);
+    printf(" >  Fseries           ");
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BLACK_PEN);
+}
+
+void pattern()
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_GREEN);
+    gotoxy(48, 8);
+    printf(" >  Pattern           ");
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BLACK_PEN);
+}
+
+void mainMenu()
+{
+    gotoxy(47, 5);
+    printf("╔══════════════════════╗\n");
+    gotoxy(47, 6);
+    printf("║      MAIN MENU       ║\n");
+    gotoxy(47, 7);
+    printf("║══════════════════════║\n");
+    gotoxy(47, 8);
+    printf("║ 1. Pattern           ║\n");
+    gotoxy(47, 9);
+    printf("║ 2. Fseries           ║\n");
+    gotoxy(47, 10);
+    printf("║ 3. Calculator        ║\n");
+    gotoxy(47, 11);
+    printf("║ 4. End Program       ║\n");
+    gotoxy(47, 12);
+    printf("╚══════════════════════╝\n");
+}
+
+void gotoxy(int x, int y)
+{
+    COORD c = {x, y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}    
